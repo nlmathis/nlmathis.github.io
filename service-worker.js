8a84bb62fdd32dbd490d13dcfd7f68ebe9c15e9a
nathan <nmathisak@gmail.com>
2019-10-03 21:24:49 -0500

    fix fetch

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
+let fileCacheName = 'files';
+
 self.addEventListener('install', event => {
     console.log('Service worker installing...');
     event.waitUntil(
-        caches.open('files').then(function(cache) {
+        caches.open(fileCacheName).then(function(cache) {
           return cache.addAll(
             [
               '/images/small-icon.png',
@@ -21,7 +23,17 @@ self.addEventListener('activate', event => {
     console.log('Service worker activating...');
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log('fetch event' + event);
-    event.respondWith(caches.match(event.request));
-  });
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(fileCacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
